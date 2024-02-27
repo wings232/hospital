af63571ca9c41cf00f438d6130e909013193350c
wings232 <a.jaheer786@gmail.com>
2024-02-27 12:41:18 +0530

    Add files via upload

---
+++ ajax/dashboard/myprofile.php
@@ -0,0 +1,80 @@
+<?php
+	//echo "user Dashboard";
+?>
+<div class="user_nav">
+	<ul>
+		<li><i class="fa fa-home"></i></li>
+		<li>Accounts</li>
+		<li>My Profile</li>
+	</ul>
+</div>
+<div class="profile_list">
+	<div class="profile_set">
+		<ul>
+			<li>
+				<div class="column_l">
+					Name
+				</div>
+				<div class="column_r">
+					Jaheer Hussain A
+				</div>
+			</li>
+			<li>
+				<div class="column_l">
+					Email
+				</div>
+				<div class="column_r">
+					jaheerabdul@techshuttle.com
+				</div>
+			</li>
+			<li>
+				<div class="column_l">
+					Mobile No
+				</div>
+				<div class="column_r">
+					9094946095
+				</div>
+			</li>
+			<li>
+				<div class="column_l">
+					Gender
+				</div>
+				<div class="column_r">
+					Male
+				</div>
+			</li>
+			<li>
+				<div class="column_l">
+					City
+				</div>
+				<div class="column_r">
+					Chennai
+				</div>
+			</li>
+			<li>
+				<div class="column_l">
+					State
+				</div>
+				<div class="column_r">
+					Tamil Nadu
+				</div>
+			</li>
+			<li>
+				<div class="column_l">
+					Country
+				</div>
+				<div class="column_r">
+					India
+				</div>
+			</li>
+			<li>
+				<div class="column_l">
+					Enrolled Courses
+				</div>
+				<div class="column_r">
+					5 Courses
+				</div>
+			</li>
+		</ul>
+	</div>
+</div>
